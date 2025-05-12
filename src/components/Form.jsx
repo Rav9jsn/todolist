6f0fdc47e2230897ae3d1d6ac12ed94d36d6d738
Rav9jsn <kravi402253@gmail.com>
2025-05-12 18:18:42 +0530

    Responsivness Completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,25 +7,41 @@ const Form = ({ todos, delettodo, setid }) => {
         return (
           <>
             <div
-              className="mt-16 border-b-[0.00010rem] flex mx-auto w-[80vw] mb-5 justify-evenly "
+              className="my-10 pb-3  flex flex-col gap-1  sm:px-[5vw] pl-[5vw]  "
               key={d.id}
             >
-              <p className="py-1">{d.title}</p>
-              <p className="py-1">{d.description}</p>
-              <div className="flex gap-3">
-                <button
-                  onClick={() => setid(d.id)}
-                  className="uppercase mb-2 p-[9px] hover:text-black text-white font-bold bg-yellow-600 rounded-md"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => delettodo(d.id)}
-                  className="uppercase mb-2 p-[9px] hover:text-black text-white font-bold bg-red-600 rounded-md"
-                >
-                  delete
-                </button>
+              <div className="flex font-semibold border-b-[0.00010rem] uppercase gap-[15px] sm:gap-[133px]">
+                {" "}
+                <p className="underline inline text-[#7779de]"> Tilte:-</p>{" "}
+                <p className="font-semibold capitalize">{d.title}</p>
               </div>
+              <div className="flex border-b-[0.00010rem] gap-[10px] uppercase font-semibold sm:gap-[70px]">
+                {" "}
+                <p className="underline sm:block hidden font-semibold">
+                  Description:-
+                </p>
+                <p className="underline sm:hidden font-semibold">Note:-</p>
+                <p className=" capitalize">{d.description}</p>
+              </div>
+              <div className=" uppercase  mt-2 flex gap-[10px] sm:gap-[111px]">
+                {" "}
+                <p className=" underline font-semibold"> Action</p>{" "}
+                <div className="flex gap-3 sm:mr-0 mr-3 h-[6vh]">
+                  <button
+                    onClick={() => setid(d.id)}
+                    className="uppercase  px-[9px] hover:text-black text-white font-bold bg-yellow-600 rounded-md"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => delettodo(d.id)}
+                    className="uppercase  px-[5px] hover:text-black text-white font-bold bg-red-600 rounded-md"
+                  >
+                    delete
+                  </button>
+                </div>
+              </div>
+              <hr />
             </div>
           </>
         );
--- src/components/Ttododata.jsx
+++ src/components/Ttododata.jsx
@@ -40,21 +40,24 @@ const Ttododata = ({ setTodosData, todos, id, setId }) => {
 
   return (
     <>
-      <form onSubmit={onsubmitListner} className="flex justify-center ">
+      <form
+        onSubmit={onsubmitListner}
+        className="flex lg:flex-row flex-col items-center gap-3 justify-center "
+      >
         {" "}
         <input
           type="text"
           value={title}
           placeholder="Title"
           onChange={(e) => setTitle(e.target.value)}
-          className="p-2 h-[2rem] w-[20vw] mr-4 focus:outline-none text-black my-auto"
+          className="p-2 h-[2rem] lg:w-[20vw] w-[19.5rem] lg:mr-4 focus:outline-none text-black my-auto"
         />
         <input
           type="text"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
           placeholder="Description"
-          className="p-2 h-[2rem] w-[20vw] focus:outline-none text-black  my-auto "
+          className="p-2 h-[2rem] lg:w-[20vw] w-[19.5rem] focus:outline-none text-black  my-auto "
         />
         {id && (
           <button className="uppercase animate-bounce duration-100 ` bg-yellow-400 p-2 w-[70px]  ml-2 rounded-md text-white font-[700] ">
@@ -67,12 +70,6 @@ const Ttododata = ({ setTodosData, todos, id, setId }) => {
           </button>
         )}
       </form>
-
-      <div className=" pb-2 uppercase font-bold mt-16 border-b-[0.0010rem] w-[80vw] mx-auto flex justify-evenly">
-        <p> Tilte</p>
-        <p>Description</p>
-        <p> action</p>
-      </div>
     </>
   );
 };
